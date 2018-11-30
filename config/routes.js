e8f2858f7a001ccd6502624538ee8a0ebb3b9f46
Brad Mortensen <b.mort86@gmail.com>
2018-11-30 09:38:54 -0700

    Initial Commit

--- config/routes.js
+++ config/routes.js
@@ -10,6 +10,16 @@ module.exports = server => {
 
 function register(req, res) {
   // implement user registration
+  axios
+  .post(
+    'https://08ad1pao69.execute-api.us-east-1.amazonaws.com/dev/random_ten'
+  )
+  .then(response => {
+    res.status(200).json(response.data);
+  })
+  .catch(err => {
+    res.status(500).json({ message: 'Error Fetching Jokes', error: err });
+  });
 }
 
 function login(req, res) {
